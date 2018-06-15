00f9e7cc5408c172745aadfef849d1ee41010f83
Piotr Cyrzan <p.cyrzan@gmail.com>
2018-06-15 14:09:16 +0200

    Mój kod

--- ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
+++ ProjectEuler/TheProjectEulerLibrary/EulerLibrary2/PasswordValidator/PasswordValidatorTests.cs
@@ -28,13 +28,61 @@ namespace EulerLibrary2.PasswordValidator
         }
 
         [Test]
-        public void IsExceptionHandling()
+        public void IsPasswordValid_OneCharacterPassword_ThrowsTooShort()
         {
             var password = "A";
             var exception = Assert.Throws<ApplicationException>(
                 () => this.validator.IsPasswordValid(password));
-            var expectedMessage = "x";
+            var expectedMessage = "Password is too short. It needs to be at least 15 characters long";
             Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void IsPasswordValid_OnlyOneDigit_ThrowsNeedsMinimumTwoDigits()
+        {
+            var password = "HiMyNameIsPiotr1";
+            var exception = Assert.Throws<ApplicationException>(
+                () => this.validator.IsPasswordValid(password));
+            var expectedMessage = "Password require at least 2 digits, but got 1";
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void IsPasswordValid_NoSpecialChar_ThrowsNeedSpecialChar()
+        {
+            var password = "HiMyNameIsPiotr12";
+            var exception = Assert.Throws<ApplicationException>(
+                () => this.validator.IsPasswordValid(password));
+            var expectedMessage = "Special characters are missing";
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void IsPasswordValid_NotEnoughUpperCase_ThrowsNeedMinimumTwoUpperCaseLetters()
+        {
+            var password = "#Himynameispiotr12";
+            var exception = Assert.Throws<ApplicationException>(
+                () => this.validator.IsPasswordValid(password));
+            var expectedMessage = "At least 2 upper case letters needs to be present";
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void IsPasswordValid_ConsecutiveCharacters_ThrowsCannotHaveConsecutiveCharacters()
+        {
+            var password = "#HiMyNameIsPiotrr12";
+            var exception = Assert.Throws<ApplicationException>(
+                () => this.validator.IsPasswordValid(password));
+            var expectedMessage = "Duplicated character: rr";
+            Assert.AreEqual(expectedMessage, exception.Message);
+        }
+
+        [Test]
+        public void IsPasswordValid()
+        {
+            var password = "%ążźćńśęćńżśęóńó98";
+            var isPasswordValid = this.validator.IsPasswordValid(password);
+            Assert.IsTrue(isPasswordValid);
         }
     }
 }
